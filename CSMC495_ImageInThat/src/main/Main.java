658694835a2849b8e31dfebf88cb38b644a97b3b
lovenirds <lovenirds@gmail.com>
2014-11-18 11:55:12 +0100

    Establish the master.

--- CSMC495_ImageInThat/src/main/Main.java
+++ CSMC495_ImageInThat/src/main/Main.java
@@ -1,6 +1,9 @@
 package main;
 
 public class Main {
-//test
-	//here too
+/*
+ * This is just the master for now, without any functionality.
+ * We can each branch off the master to start our code and push those
+ * branches into the master when we want.
+ */
 }
