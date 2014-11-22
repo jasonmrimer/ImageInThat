db203d17555fb8c5b07247e45efe0aa678434a46
lovenirds <lovenirds@gmail.com>
2014-11-22 20:12:04 +0100

    Merging [without using branch due to problems]. IG creates random shapes but repaints with every window change.

--- CSMC495_ImageInThat/src/main/Main.java
+++ CSMC495_ImageInThat/src/main/Main.java
@@ -1,6 +1,10 @@
 package main;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.GeneralPath;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -16,19 +20,21 @@ public class Main extends JPanel {
 	static JPanel panel;
 	
 	public static void main(String arg[]){
-		ImageGenerator ig = new ImageGenerator(100, 100);
 		JFrame frame = new JFrame();
 		frame.getContentPane().add(new Main());
-		frame.setSize(100, 100);
+		frame.setSize(400, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		ig.getImage().getGraphics().drawImage(ig.getImage(), 1, 1, null, frame);
-		ig.repaint();
 	}
 	
 	public void paint(Graphics g){
-		ImageGenerator ig = new ImageGenerator(100, 100);
+		Graphics2D g2D = (Graphics2D) g;
+		ImageGenerator ig = new ImageGenerator(400, 400);
+		GeneralPath polygon = new GeneralPath();
 		g.drawImage(ig.getImage(), 0, 0, this);
+		g2D.setPaint(Color.black);
+		g2D.fill(ig.getPolygon());
+		g2D.draw(ig.getPolygon());
 	}
 	
 	
